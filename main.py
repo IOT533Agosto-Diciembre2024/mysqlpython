05e1708e990101a0b9c881b464285b3aa67eee4e
Andrés Torres <atorres@applab.mx>
2024-11-13 10:41:01 -0600

    MQTT

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-import mysql.connector
-from mysql.connector import Error
+import time
+
 from db_manager import DBManager
+import paho.mqtt.client as mqtt
+
+db_manager = DBManager('10.48.209.159', 'proyecto_xrover', 'profe', 'andrestorres')
 
 
-def desplegar_menu():
-    print("0) Salir \n1) Insertar director \n2) Ver directores")
+def on_message(client, userdata, msg):
+    msg = msg.payload.decode()
+    msg_split = msg.split("_")
+    sensor = msg_split[0]
+    valor = msg_split[1]
+    #Insertar valor con db manager a la tabla del sensor
+    #db_manager.insert_value(valor)
+    print(f"El sensor es: {sensor} valor: {valor}")
 
 
-desplegar_menu()
-option = input("Ingresa la opción: ")
-db_manager = DBManager()
-while option != "0":
-    if option == "1":
-        director_no = input("Ingresa el número de director: ")
-        director_name = input("Ingresa el nombre del director: ")
-        db_manager.insert_director(director_no, director_name)
-    if option == "2":
-        db_manager.show_directors()
-    desplegar_menu()
-    option = input("Ingresa la opción: ")
+unacked_publish = set()
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+mqtt_client.on_message = on_message
+
+mqtt_client.connect("broker.hivemq.com",1883)
+mqtt_client.loop_start()
+mqtt_client.subscribe("weccat/ultrasonido")
+
+try:
+    print("Esperando mensajes")
+    while True:
+        time.sleep(1)
+except:
+    print(f"Ocurrio un error")
+finally:
+    mqtt_client.loop_stop()
+    mqtt_client.disconnect()
+
+
+
+
+
